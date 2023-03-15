4c664377ba9f0fa3c8566165df08d54134f9861b
EC2 Default User <ec2-user@ip-172-31-22-206.ap-northeast-1.compute.internal>
2023-03-15 08:31:59 +0000

    [Fin]5-18/コメント機能実装/余計な記述までついてしまうので要修正/bookers2になってから聞く

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   root to: "homes#top"
   get "/top" => 'homes#top'
   get "homes/about", as: "about"
-  resources :post_images, only: [:new,:create, :index, :show, :destroy]
+  resources :post_images, only: [:new,:create, :index, :show, :destroy] do
+    resources :post_comments, only:[:create, :destroy]
+  end
   resources :users, only: [:show, :edit, :update]
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
