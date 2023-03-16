a977c7d0f17af6e1d90fca408c4dd56159a9fc3d
EC2 Default User <ec2-user@ip-172-31-22-206.ap-northeast-1.compute.internal>
2023-03-16 04:30:00 +0000

    [Fin]5-19/いいね機能の実装

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -2,6 +2,7 @@ class PostImage < ApplicationRecord
   has_one_attached :image
   belongs_to :user
   has_many :post_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
 
   def get_image
     unless image.attached?  #imageが存在しない時
@@ -10,4 +11,9 @@ class PostImage < ApplicationRecord
     end
     image
   end
+
+  def favorited_by?(user)
+    favorites.exists?(user_id: user.id)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get "homes/about", as: "about"
   resources :post_images, only: [:new,:create, :index, :show, :destroy] do
     resources :post_comments, only:[:create, :destroy]
+    resources :favorites, only:[:create, :destroy]
   end
   resources :users, only: [:show, :edit, :update]
 end
